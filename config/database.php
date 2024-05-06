24c44f8c91757c3974c5f5c093c30720efcd74d9
vincentaa7 <230010009@stikom-bali.ac.id>
2024-05-07 03:12:00 +0800

    first commit

---
+++ config/database.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "db_xyz";
+
+//membuat koneksi ke database
+$connect = new mysqli($servername, $username, $password, $database);
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
